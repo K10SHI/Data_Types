1def1eca79fa125fbfde5263be533cc7baf34642
K10SHI <89117005+K10SHI@users.noreply.github.com>
2021-08-24 05:37:36 +0800

    Context Change

---
+++ Class_type.v
@@ -0,0 +1,10 @@
+class packet;
+  // Properties
+  bit [31:0] address;
+  bit [31:0] data   ;
+   
+  // Method
+  function new();
+    $display("Inside new Function of packet");
+  endfunction 
+endclass : packet
